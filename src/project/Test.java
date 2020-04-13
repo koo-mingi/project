376826f9f890f6ae946ea39a1614450faa323801
jinkyungpark <kyung5896@gmail.com>
2020-04-13 17:11:07 +0900

    재수정

--- src/project/Test.java
+++ src/project/Test.java
@@ -3,11 +3,7 @@ package project;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("테스트");
-<<<<<<< HEAD
 		System.out.println("팀장이 변경");
-=======
 		System.out.println("테스트 이정민");
-
->>>>>>> branch 'master' of https://github.com/koo-mingi/project.git
 	}
 }
