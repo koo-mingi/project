e4379f6da3490a033dc03f791499d5bb1e38f17c
admin <nmz0328@naver.com>
2020-04-13 17:07:03 +0900

    1

--- src/project/Test.java
+++ src/project/Test.java
@@ -4,14 +4,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("테스트");
-<<<<<<< HEAD
+
 		System.out.println("다롱이");
 		System.out.println("다롱이2");
 		System.out.println("다롱이3");
 		
-=======
+
 		System.out.println("테스트 이정민");
->>>>>>> branch 'master' of https://github.com/koo-mingi/project.git
+
 
 	}
 
