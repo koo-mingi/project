d9d57f50c14a38afc42614dffb87df571d672f6c
jongeunyoon <whddms87@gmail.com>
2020-04-23 13:55:47 +0900

    0423 수정

--- src/persistence/UserDAO.java
+++ src/persistence/UserDAO.java
@@ -36,6 +36,7 @@ public class UserDAO {
 	}
 //	끝 ----------------------------------------------------------------------------------------------------------------------------------------------
 	
+//	회원가입 할 때 받아오는 정보
 //	user 정보 추가 / 유저 아이디가 없으면 가입, 있으면 중복으로 불가입
 	public int insertUser(String UserID, String password, String name, String Email) {
 		int result = 0;
@@ -61,16 +62,16 @@ public class UserDAO {
 //	끝 ----------------------------------------------------------------------------------------------------------------------------------------------
 	
 
-//	로그인
-	// 입력된 DB정보를 오라클에서 ID, PW를 조회해서 일치하는지 확인, 맞거나 아니거나 할 경우 정보 전달
-	public UserVO getUser(int no) {
+//	개인정보 받기
+//	입력된 DB정보를 오라클에서 ID, PW를 조회해서 일치하는지 확인, 맞거나 아니거나 할 경우 정보 전달
+	public UserVO getUser(String id) {
 		
 		String sql="select * from userTBL where no=?";
 		UserVO vo=null;
 		try(Connection con=getConnection();
 			PreparedStatement pstmt=con.prepareStatement(sql)) {
 			
-			pstmt.setInt(1, no);
+			pstmt.setString(1, id);
 			
 			//select
 			ResultSet rs=pstmt.executeQuery();
@@ -88,4 +89,33 @@ public class UserDAO {
 	}
 //	끝 ----------------------------------------------------------------------------------------------------------------------------------------------
 	
+//	Song DB insert
+	public int getSong(int songID, String songname, int difficulty, int speed) {
+		int result = 0;
+		
+		String sql = "select * from SongTBL where songid";
+					// SQL문을 연결된 OracleDB에 PreparedStatement를 통해서 보낸다
+					// 실행하는 과정에서 오라클과 동일해야된다
+		try (Connection con = getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			pstmt.setInt(1, songID);
+			pstmt.setString(2, songname);
+			pstmt.setInt(3, difficulty);
+			pstmt.setInt(4, speed);
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+//	로그인(성공)
+		
+//	로그인(실패)
+		
+//	DB연동(insert) 실행
+		
 }
