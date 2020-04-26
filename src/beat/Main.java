5088de97616aa92f68e3cc34b67a44336e837041
ekzmf <ekzmf@MinGI>
2020-04-26 10:59:18 +0900

    ㅇㅇ

--- src/beat/Main.java
+++ src/beat/Main.java
@@ -16,7 +16,7 @@ import domain.RecodeVO;
 public class Main {
 	public static final int SCREEN_WIDTH = 800;
 	public static final int SCREEN_HEIGHT = 600;
-	public static int NOTE_SPEED = 5;		//노트 떨어지는 속도 조절
+	public static int NOTE_SPEED = 6;		//노트 떨어지는 속도 조절
 	public static final int SLEEP_TIME = 10;
 	public static final int REACH_TIME = 2;
 	public static ArrayList<RecodeVO> MYRECODE= new ArrayList<RecodeVO>(); // 개인 기록 저장
